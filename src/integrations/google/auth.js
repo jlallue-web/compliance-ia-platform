85bde2f56ad84670dd6c43e60e6e960b3ea04bb8
jlallue-web <jlallue@gmail.com>
2026-07-01 18:16:29 -0300

    Support Google credentials from environment

--- src/integrations/google/auth.js
+++ src/integrations/google/auth.js
@@ -1,12 +1,23 @@
 const { google } = require('googleapis');
 
 function getGoogleAuth() {
+  const scopes = [
+    'https://www.googleapis.com/auth/spreadsheets.readonly',
+    'https://www.googleapis.com/auth/drive.readonly'
+  ];
+
+  if (process.env.GOOGLE_SERVICE_ACCOUNT_JSON) {
+    const credentials = JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_JSON);
+
+    return new google.auth.GoogleAuth({
+      credentials,
+      scopes
+    });
+  }
+
   return new google.auth.GoogleAuth({
     keyFile: process.env.GOOGLE_APPLICATION_CREDENTIALS,
-    scopes: [
-      'https://www.googleapis.com/auth/spreadsheets.readonly',
-      'https://www.googleapis.com/auth/drive.readonly'
-    ]
+    scopes
   });
 }
 
